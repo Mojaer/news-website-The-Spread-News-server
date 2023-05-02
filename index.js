7cbd5f4565099b58d1be57bb30597904fbfd8e1e
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-02 19:29:02 +0600

    all servers are  commited

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+app = express();
+const port = process.env.PORT || 3000;
+var cors = require('cors');
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.get('/', (req, res) => {
+    res.send('news is running');
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+
+
+})
+
+app.listen(port, () => {
+    console.log(`port is running on http://localhost:${port}`)
+})
